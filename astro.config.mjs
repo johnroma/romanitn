26ca932d14b98b623e490b20cfd9d73cb7ab594a
johnroma <login@john.ro>
2024-01-10 00:42:46 +0100

    htmx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from "astro/config"
 //import node from "@astrojs/node";
 import vercel from "@astrojs/vercel/serverless"
+import htmx from "astro-htmx"
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [htmx()],
   output: "hybrid",
   adapter: vercel(),
+
   // adapter: node({
   //   mode: "standalone",
   // }),
